64c201cab65bc0b829af73e6c511a36cd2db72f1
iXavy <ict@ixavy.com>
2022-11-20 20:47:32 +0100

    Acabado ejemplo map

--- Training/Map.playground/Contents.swift
+++ Training/Map.playground/Contents.swift
@@ -14,18 +14,17 @@ print(newNumbers)
 
 // Versión CON MAP
 // El MAP es un clousure que toma como entrada los valores del array en el parámetro que le indicamos y lo transforma según el código escrito en él
+// Versión larga:
 
-let doubleNumbers = myNumbers.map { currentNumber in
+let doubleNumbers = myNumbers.map { (currentNumber) -> Int in
     return currentNumber * 2
 }
 
-// Transformando los enteros a string
-
-let strNumbers = myNumbers.map { currentNumber in
+let strNumbers = myNumbers.map { (currentNumber) -> String in
     return String(currentNumber)
 }
 
-let squareNumbers = myNumbers.map { currrentNumber in
+let squareNumbers = myNumbers.map { (currrentNumber) -> Int in
     return currrentNumber * currrentNumber
 }
 
@@ -33,8 +32,21 @@ print(doubleNumbers)
 print(strNumbers)
 print(squareNumbers)
 
-// Versiones reducidas de los dos ejemplos anteriores
+// Versión reducida
 
 print(myNumbers.map { $0 * 2})
 print(myNumbers.map { String($0)})
 print(myNumbers.map { $0 * $0})
+
+// Ejemplo de map con diccionarios
+// Hay dos parámetros de entrada: la clave y el valor ($0 y $1). El return (valor de vuelta) no puede ser un objeto, por lo tanto no puede ser un diccionario
+// Tiene que ser un tipo simple (Int, String, etc..)
+// Recordatorio: Un diccionario no tiene orden, se suele ordenar por el orden alfabético de sus claves
+
+let  family = ["Juan Gabriel":28,"Mar":20,"Juanjo":51]
+
+family.map { (familyName: String, familyAge: Int) -> String in
+    return "\(familyName) tiene \(familyAge) años."
+}
+
+print(family.map {"\($0) tiene \($1) años."})
